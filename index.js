f7c4fd24c296234095c720acbdffde5efffb2bc8
per <per7614@163.com>
2020-10-05 18:48:23 +0800

    10.5 add launch screen

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+class RootApp extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return <App />;
+  }
+}
+
+AppRegistry.registerComponent(appName, () => RootApp);
