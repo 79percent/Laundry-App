eddd2859f4f40d8da18fa11bf1da18d8d95ca452
per <per7614@163.com>
2020-10-08 22:36:31 +0800

    10.8 add login out

--- src/pages/PersonalCenter/index.js
+++ src/pages/PersonalCenter/index.js
@@ -1,10 +1,44 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
+import { connect } from 'react-redux';
+import styles from './styles';
+import { loginOut } from '../../redux/modules/userInfo/action';
 
-export default function PersonalCenter() {
+const Screen = ({ navigation, onLoginOut }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>PersonalCenter!</Text>
+    <View style={styles.container}>
+      <Button
+        onPress={onLoginOut}
+        title="退出登录"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </View>
   );
-}
+};
+
+const mapStateToProps = state => {
+  return {
+    userInfo: state.userInfo,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLoginOut: () => {
+      const action = loginOut();
+      dispatch(action);
+    },
+  };
+};
+
+const HomeScreen = connect(mapStateToProps, mapDispatchToProps)(Screen);
+
+export default HomeScreen;
