ce30f3252a2a56979995f1f64eb204de07a4426d
per <per7614@163.com>
2020-11-17 17:57:46 +0800

    11.17

--- src/pages/AddOrder/index.js
+++ src/pages/AddOrder/index.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import styles from './styles';
 
 export default function AddOrder() {
-  return <View style={{ flex: 1, backgroundColor: 'pink' }} />;
+  return (
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      locations={[0, 0.3, 1]}
+      colors={['#ccc', '#fff']}
+      style={styles.linearGradient}
+    >
+      <Text style={styles.buttonText}>Sign in with Facebook</Text>
+    </LinearGradient>
+  );
 }
---
+++ src/pages/AddOrder/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+  },
+});
