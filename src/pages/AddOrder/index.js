53efa21eb61b8040d15bf14a9f4babb842ad6e79
per <per7614@163.com>
2020-11-20 17:57:03 +0800

    11.20 填写订单

--- src/pages/AddOrder/index.js
+++ src/pages/AddOrder/index.js
@@ -8,11 +8,25 @@ export default function AddOrder() {
     <LinearGradient
       start={{ x: 0, y: 0 }}
       end={{ x: 0, y: 1 }}
-      locations={[0, 0.3, 1]}
+      locations={[0, 0.7, 1]}
       colors={['#ccc', '#fff']}
       style={styles.linearGradient}
     >
-      <Text style={styles.buttonText}>Sign in with Facebook</Text>
+      <View style={styles.card1}>
+        <View style={styles.card1TopSelect}>
+          <View style={styles.card1TopSelect1}>
+            <View style={styles.card1TopSelect1Content}>
+              <Text style={styles.card1TopSelect1ContentText}>上门取件</Text>
+            </View>
+          </View>
+          <View style={styles.card1TopSelect2}>
+            <View style={styles.card1TopSelect2Content}>
+              <Text style={styles.card1TopSelect2ContentText}>自取送</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.card1SiteSelect} />
+      </View>
     </LinearGradient>
   );
 }
--- src/pages/AddOrder/styles.js
+++ src/pages/AddOrder/styles.js
@@ -1,15 +1,64 @@
 import { StyleSheet } from 'react-native';
+import { pixelX, pixelY, windowW } from '../../utils';
 
 export default StyleSheet.create({
   linearGradient: {
     flex: 1,
+    alignItems: 'center',
   },
-  buttonText: {
-    fontSize: 18,
-    fontFamily: 'Gill Sans',
-    textAlign: 'center',
-    margin: 10,
-    color: '#ffffff',
-    backgroundColor: 'transparent',
+  card1: {
+    width: windowW - pixelX(20),
+    height: pixelY(200),
+    marginTop: pixelY(20),
+  },
+  card1TopSelect: {
+    width: '100%',
+    flexDirection: 'row',
+    height: pixelY(40),
+  },
+  card1SiteSelect: {
+    width: '100%',
+    height: pixelY(160),
+    backgroundColor: '#fff',
+    borderTopLeftRadius: pixelX(10),
+    borderBottomLeftRadius: pixelX(10),
+    borderBottomRightRadius: pixelX(10),
+  },
+  card1TopSelect1: {
+    flex: 1,
+    paddingTop: pixelX(5),
+    paddingLeft: pixelX(10),
+    paddingRight: pixelX(5),
+  },
+  card1TopSelect2: {
+    flex: 1,
+    paddingLeft: pixelX(5),
+    paddingRight: pixelX(0),
+  },
+  card1TopSelect1Content: {
+    backgroundColor: '#E0F5FA',
+    width: '100%',
+    height: '100%',
+    borderTopLeftRadius: pixelX(10),
+    borderTopRightRadius: pixelX(10),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card1TopSelect1ContentText: {
+    color: '#000',
+    fontSize: pixelX(16),
+  },
+  card1TopSelect2Content: {
+    backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
+    borderTopLeftRadius: pixelX(10),
+    borderTopRightRadius: pixelX(10),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card1TopSelect2ContentText: {
+    color: '#34A0ED',
+    fontSize: pixelX(16),
   },
 });
